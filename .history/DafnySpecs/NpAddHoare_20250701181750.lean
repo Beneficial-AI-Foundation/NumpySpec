92acf4fc280e8e881179ad74ad687eb49c022729
Lord-Van <2200017789@stu.pku.edu.cn>
2025-07-02 11:02:27 -0400

    Delete unecessary files

--- .history/DafnySpecs/NpAddHoare_20250701181750.lean
+++
@@ -1,9 +0,0 @@
-import Std.Tactic.Do
-
-
-open Std.Do
-
-theorem mkFreshNat_spec [Monad m] [WPMonad m sh] :
-  ⦃⌜#fst = n ∧ #snd = o⌝⦄
-  (mkFreshNat : StateT (Nat × Nat) m Nat)
-  ⦃⇓ r => ⌜r = n ∧ #fst = n + 1 ∧ #snd = o⌝⦄
